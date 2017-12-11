84930497c0524fb1edc99c3839c3c8e4b7fb87d0
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2017-12-11 21:20:53 +0300

    Create controller for start window

---
+++ Project/CheerfulGarden/src/main/java/cheerfulgarden/controller/StartWindowController.java
@@ -0,0 +1,37 @@
+package cheerfulgarden.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class StartWindowController {
+    @FXML
+    Button productButton;
+
+    @FXML
+    private void initialize(){
+        productButton = new Button();
+    }
+
+    public void chooseProduct() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/application.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root, 350, 450));
+            stage.setTitle("Веселый огород");
+            stage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void chooseExit() {
+        System.exit(0);
+    }
+
+}
